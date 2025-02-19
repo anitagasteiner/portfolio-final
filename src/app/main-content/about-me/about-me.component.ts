c285cd4104098c92c40bc3eb247c447502d8f3a3
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-02-19 13:19:04 +0100

    arrow movement in progress

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { GeneralService } from '../../general.service';
 
 @Component({
@@ -8,8 +8,38 @@ import { GeneralService } from '../../general.service';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
 
   generalService = inject(GeneralService);
+
+  arrowOptions = {
+    threshold: 1
+  };
+
+  arrowObserver: IntersectionObserver | undefined;
+
+  ngAfterViewInit(): void {    
+    this.arrowObserver = new IntersectionObserver(this.callback, this.arrowOptions);
+    const arrow = document.querySelector('#about-me-arrow');
+    if (arrow) {
+      this.arrowObserver.observe(arrow);
+    }
+  }
+
+  callback = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(entry => {
+      console.log('About-Arrow: ', entry.isIntersecting);
+      if (entry.isIntersecting) {
+        this.addClassName('move');
+      }
+    });
+  };
+
+  addClassName(className: string): void {
+    const arrow = document.querySelector('#about-me-arrow');
+    if (arrow) {
+      arrow.classList.add(className);      
+    }
+  }
 
 }

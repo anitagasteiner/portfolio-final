bb12290f054363c232aca76b02d80ec15079a794
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-03-21 13:06:47 +0100

    imprint ngx-translate completed

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { GeneralService } from './../general.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    TranslatePipe
   ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'

0f2eb2e18f5cba9f9bb0c6c838cd325909d5bd08
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-03-24 09:03:58 +0100

    language switch options in header optimized

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { GeneralService } from '../../../general.service';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import translationsDE from './../../../../assets/i18n/de.json';
 import translationsEN from './../../../../assets/i18n/en.json';
 
@@ -12,41 +12,60 @@ import translationsEN from './../../../../assets/i18n/en.json';
   imports: [
     CommonModule,
     RouterLink,
-    RouterLinkActive
-    // TranslatePipe
+    RouterLinkActive,
+    TranslatePipe
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
 
+  generalService = inject(GeneralService);
+
   constructor(private translate: TranslateService) {
-    this.translate.addLangs(['de', 'en']);
-    this.translate.setDefaultLang('de');
+    this.translate.addLangs(['de', 'en']);  
+    this.translate.setDefaultLang('de');  
     this.translate.setTranslation('de', translationsDE);
     this.translate.setTranslation('en', translationsEN);
-    this.translate.use('de');
+    this.setLanguage();    
   }
 
   ngOnInit() {
     this.generalService.getLanguageFromLocalStorage();
-    this.setBtnLanguage();
+    // this.setBtnLanguage();
   }
 
-  generalService = inject(GeneralService);
+  // translateLanguageTo(language: string) {
+  //   this.translate.use(language);
+  // }
 
-  languageSwitch: string = 'English';
-
-  translateLanguageTo(language: string) {
-    this.translate.use(language);
+  setLanguage() {
+    if (this.generalService.currentLanguage == 'de') {      
+      this.translate.use('de');
+    } else if (this.generalService.currentLanguage == 'en') {
+      this.translate.use('en');
+    }    
+    // const language = localStorage.getItem('language');
+    // if (language == 'de') {
+    //   this.translate.setDefaultLang('de');
+    //   this.translate.use('de');
+    // } else if (language == 'en') {
+    //   this.translate.setDefaultLang('en');
+    //   this.translate.use('en');
+    // } else {
+    //   this.translate.setDefaultLang('de');
+    //   this.translate.use('de');
+    // }    
   }
 
   setBtnLanguage() {
     const language = localStorage.getItem('language');
     if (language == 'de') {
-      this.languageSwitch = 'English';
+      // this.languageSwitch = 'English';
+      this.translate.use('en');
     } else if (language == 'en') {
-      this.languageSwitch = 'Deutsch';
+      // this.languageSwitch = 'Deutsch';
+      this.translate.use('de');
     }
   }
 
@@ -57,7 +76,7 @@ export class HeaderComponent implements OnInit {
       localStorage.setItem('language', 'de');
     }
     this.generalService.getLanguageFromLocalStorage();
-    this.setBtnLanguage();
+    this.setLanguage();
     this.generalService.setBtnText();
     this.generalService.setPlaceholders();
   }

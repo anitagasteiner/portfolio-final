9aead4831c071686abf8f9327db270a344965a85
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-03-24 09:17:52 +0100

    language switch in title resolved

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GeneralService } from '../../general.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+
+  generalService = inject(GeneralService);
 
 }
